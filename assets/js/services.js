d23d4db259a71c859fa9017895a8e4851ae0325e
skyler <skyler.blumenthal@gmail.com>
2018-05-22 14:27:44 -0600

    Putting in tabs, fixing homepage youtube vid

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,20 +1,22 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function currentDiv(n) {
-  showDivs((slideIndex = n));
-}
+// function currentDiv(n) {
+//   showDivs((slideIndex = n));
+// }
 
-function showDivs(n) {
+function showDivs(evt, tabnum, tabSection) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  tablink = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablink.length; i++) {
+    tablink[i].className = tablink[i].className.replace(" active", "");
+
+
   }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active-tab";
+  document.getElementById(tabnum).style.display = "block";
+  document.getElementById(tabSection).className += " active";
 }
